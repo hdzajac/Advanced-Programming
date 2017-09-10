da6b455f6f9023a238a8203658db9fda250a8edc
hubz00 <hubert.zajac@outlook.com>
2017-09-10 14:26:47 +0200

    curve definition

--- curves.hs
+++ curves.hs
@@ -11,6 +11,9 @@ pointX (Point x _) = x
 pointY :: Point -> Double
 pointY (Point _ y) = y
 
+curve :: Point -> [Point] -> Curve
+curve p pl = (Curve p pl)
+
 
 
 
@@ -20,12 +23,16 @@ instance Eq Point where
 main :: IO ()
 
 main = do let p1 = point (0.001, 1.001)
-              p2 = point (0.000, 1.00)
+              p2 = point (0.500, 1.50)
+              p3 = point (2.000, 2.00)
               x1 = pointX p1
               y1 = pointY p1
+              c = curve p1 [p2,p3]
               result = p1 == p2
           putStrLn $ "Initial point: " ++ show p1
           putStrLn $ "x: " ++ show x1
           putStrLn $ "y: " ++ show y1
           putStrLn $ "EQ: " ++ show result
+          putStrLn $ "Curve: " ++ show c
+
 
