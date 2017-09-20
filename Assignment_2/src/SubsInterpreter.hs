b76a8b93c259fcd4112b82a61b5d4893e0515834
hubz00 <hubert.zajac@outlook.com>
2017-09-20 11:05:32 +0200

    style errors

--- Assignment_2/src/SubsInterpreter.hs
+++ Assignment_2/src/SubsInterpreter.hs
@@ -25,10 +25,10 @@ data Value = IntVal Int
 
 
 type Error = String
-type Env = Map Ident Value --variable aka symb table
-type Primitive = [Value] -> Either Error Value  --preexisting func
-type PEnv = Map FunName Primitive  --primitive env 
-type Context = (Env, PEnv) 
+type Env = Map Ident Value -- variable aka symb table
+type Primitive = [Value] -> Either Error Value  -- preexisting func
+type PEnv = Map FunName Primitive  --primitive env
+type Context = (Env, PEnv)
 
 initialContext :: Context
 initialContext = (Map.empty, initialPEnv)
@@ -44,15 +44,15 @@ initialContext = (Map.empty, initialPEnv)
 
 newtype SubsM a = SubsM {runSubsM :: Context -> Either Error (a, Env)}
 
---newtype SubsM a = SubsM {runSubsM :: Context -> Either Error (a, Env)}
+-- newtype SubsM a = SubsM {runSubsM :: Context -> Either Error (a, Env)}
 instance Functor SubsM where
-  --(a->b) -> M a -> M b
-  --(a->b) -> ((Env, PEnv) -> Either Error (a,Env)) -> ((Env, PEnv) -> Either Error (b,Env))
-  ---fmap f mf =  SubsM (\(e,pe0)-> (case (runSubsM mf (e,pe0))  of
-  ---                                  Left errMsg -> Left errMsg
-  ---                                  Right (a,e) -> Right ((f a),e))) 
+  -- (a->b) -> M a -> M b
+  -- (a->b) -> ((Env, PEnv) -> Either Error (a,Env)) -> ((Env, PEnv) -> Either Error (b,Env))
+  -- -fmap f mf =  SubsM (\(e,pe0)-> (case (runSubsM mf (e,pe0))  of
+  -- -                                  Left errMsg -> Left errMsg
+  -- -                                  Right (a,e) -> Right ((f a),e)))
   fmap = liftM
-  
+
 
 instance Applicative SubsM where
   pure = return
@@ -61,25 +61,25 @@ instance Applicative SubsM where
 instance Monad SubsM where
   -- return a :: a->SubsM ((Env, PEnv) -> Either Error (a,Env) )
   -- define cases for either
-  return x = SubsM (\(e,pe) -> Right (x,e))
-  -- f >>= m ::  SubsM(Env, PEnv) -> Either Error (a,Env) 
+  return x = SubsM (\(e,_) -> Right (x,e))
+  -- f >>= m ::  SubsM(Env, PEnv) -> Either Error (a,Env)
   --              -> (a -> SubsM ((Env, PEnv) -> Either Error (b,Env) ))
   --              -> SubsM (Env, PEnv) -> Either Error (b,Env)
-  f >>= m = SubsM (\(e0,ep0) -> case (runSubsM f (e0,ep0)) of
-                                        Left errMsg -> Left errMsg
-                                        Right (a,e) -> runSubsM (m a) (e,ep0) )
-  fail s = SubsM (\e-> Left s)
-  
+  f >>= m = SubsM (\(e0,ep0) -> case runSubsM f (e0,ep0) of
+                                  Left errMsg -> Left errMsg
+                                  Right (a,e) -> runSubsM (m a) (e,ep0))
+  fail s = SubsM (\_-> Left s)
+
 mkArray :: Primitive
-mkArray [IntVal n] 
+mkArray [IntVal n]
   | n >= 0 = return $ ArrayVal (replicate n UndefinedVal)
 mkArray _ = Left "Array() called with wrong number or type of arguments"
 
 add :: Primitive
 add [IntVal a, IntVal b] = return $ IntVal (a + b)
 add [StringVal a, StringVal b] = return $ StringVal (a ++ b)
-add [IntVal a, StringVal b] = return $ StringVal ((show a) ++ b)
-add [StringVal a, IntVal b] = return $ StringVal (a ++ (show b))
+add [IntVal a, StringVal b] = return $ StringVal (show a ++ b)
+add [StringVal a, IntVal b] = return $ StringVal (a ++ show b)
 add _ = Left "\"+\" applied to incompatible types, try any combination of String and Int"
 
 minus :: Primitive
@@ -91,77 +91,78 @@ multiply [IntVal a, IntVal b] = return $ IntVal (a * b)
 multiply _ = Left "\"*\" applied to incompatible types, try: Int * Int"
 
 modulo :: Primitive
-modulo [IntVal a, IntVal 0] = Left "\"% 0\" is undefined, try different value"
+modulo [IntVal _, IntVal 0] = Left "\"% 0\" is undefined, try different value"
 modulo [IntVal a, IntVal b] = return $ IntVal (a `mod` b)
 modulo _ = Left "\"*\" applied to incompatible types, try: Int % Int"
 
--------------------------------------------------------------------------------
---The === operator compares its arguments for structural equality, without any coercions. ??????? what does this mean
---It accepts operands of any type, but comparison of, e.g., a string and a number
---will always yield false.
+-- -----------------------------------------------------------------------------
+-- The === operator compares its arguments for structural equality, without any coercions. ??????? what does this mean
+-- It accepts operands of any type, but comparison of, e.g., a string and a number
+-- will always yield false.
 equality::[Value] -> Either Error Value
-equality [IntVal a ,IntVal b] = if(a==b) then Right(TrueVal)
-                     else Right(FalseVal)
-equality [UndefinedVal,UndefinedVal] = Right(TrueVal)
-equality [TrueVal,TrueVal] = Right(TrueVal)
-equality [FalseVal,FalseVal] = Right(TrueVal)
-equality [TrueVal,FalseVal] = Right(FalseVal)
-equality [FalseVal,TrueVal] = Right(FalseVal)
-equality [(ArrayVal []), (ArrayVal [])] = Right(TrueVal)
-equality [(ArrayVal _), (ArrayVal [])] = Left "Mismatched Sizes"
-equality [(ArrayVal []), (ArrayVal _)] = Left "Mismatched Sizes"
-equality [(ArrayVal (h1:t1)), (ArrayVal (h2:t2))] = case equality [h1,h2] of 
-                                                      Right(TrueVal) -> equality [ArrayVal (t1), ArrayVal (t2)]
-                                                      otherwise -> Left "Not Equal"
+equality [IntVal a ,IntVal b] = if a == b then Right TrueVal
+                     else Right FalseVal
+equality [UndefinedVal,UndefinedVal] = Right TrueVal
+equality [TrueVal,TrueVal] = Right TrueVal
+equality [FalseVal,FalseVal] = Right TrueVal
+equality [TrueVal,FalseVal] = Right FalseVal
+equality [FalseVal,TrueVal] = Right FalseVal
+equality [ArrayVal [], ArrayVal []] = Right TrueVal
+equality [ArrayVal _, ArrayVal []] = Left "Mismatched Sizes"
+equality [ArrayVal [], ArrayVal _] = Left "Mismatched Sizes"
+equality [ArrayVal (h1:t1), ArrayVal (h2:t2)] = case equality [h1,h2] of 
+                                                      Right TrueVal -> equality [ArrayVal t1, ArrayVal t2]
+                                                      Left message -> Left message
+                                                      _ -> Left "Not equal"
 
-equality [StringVal s1,StringVal s2] = if(s1==s2) then Right(TrueVal)
-                              else Right(FalseVal)
+equality [StringVal s1,StringVal s2] = if s1 == s2 then Right TrueVal
+                              else Right FalseVal
 equality [_] = Left "Too few arguments"
-equality [_,_] = Left "Types do not match"
-equality e = Left "Too many arguments"
+equality [_, _] = Left "Types do not match"
+equality _ = Left "Too many arguments"
 
 ----------------------------------------------------------------------------
---On the other hand, the two arguments to the < operator
---must either both be integers or both be strings, where strings are compared using
---the usual lexicographic order.
+-- On the other hand, the two arguments to the < operator
+-- must either both be integers or both be strings, where strings are compared using
+-- the usual lexicographic order.
 less::[Value] -> Either Error Value
-less [IntVal a ,IntVal b] = if(a<b) then Right(TrueVal)
-                  else Right(FalseVal)
-less [StringVal s1,StringVal s2] = if(s1<s2) then Right(TrueVal)
-                           else Right(FalseVal)
-less l = Left "Invalid comparison"
+less [IntVal a ,IntVal b] = if a < b then Right TrueVal
+                  else Right FalseVal
+less [StringVal s1,StringVal s2] = if s1 < s2 then Right TrueVal
+                                    else Right FalseVal
+less _ = Left "Invalid comparison"
 
---let (Writer (y, v')) = f x in
---Writer (y, v `mappend` v')
--- is this applied on   
---should replace the variable environment with the result of applying f to it.
-modifyEnv :: (Env -> Env) -> SubsM ()
-modifyEnv f = SubsM (\(e0,pe0) ->  Right((),(f e0)))  
+-- let (Writer (y, v')) = f x in
+-- Writer (y, v `mappend` v')
+-- is this applied on
+-- should replace the variable environment with the result of applying f to it.
+-- modifyEnv :: (Env -> Env) -> SubsM ()
+-- modifyEnv f = SubsM (\(e0,_) ->  Right((),(f e0)))
 
---should set the value of the variable i to v in the current environment
+-- should set the value of the variable i to v in the current environment
 putVar :: Ident -> Value -> SubsM ()
-putVar name val = SubsM (\(e0,pe0) ->  Right((),(Map.insert name val e0)))
+putVar name val = SubsM (\(e0,_) -> Right((), Map.insert name val e0))
 
---helper function
---should read the value of the variable i in the current environment
+-- helper function
+-- should read the value of the variable i in the current environment
 getVar :: Ident -> SubsM Value
-getVar name =  SubsM (\(e0,pe0) ->  case (lookup name  $ Map.assocs  e0)  of
-                                           Nothing -> Left ("unbound variable:"++name)
-                                           Just v -> Right (v,e0))
-										   
+getVar name =  SubsM (\(e0,_) -> case lookup name $ Map.assocs e0 of
+                                    Nothing -> Left ("unbound variable:"++name)
+                                    Just v -> Right (v,e0))
 
---Primitive::[Value] -> Either Error Value
---SubsM {runSubsM :: Context -> Either Error (a, Env)}
---SubsM {runSubsM :: Context -> Either Error (Primitive, Env)}
---SubsM {runSubsM :: Context -> Either Error (Primitive, Env)}
---helper function
---should look up the function implementing primitive i.
+
+-- Primitive::[Value] -> Either Error Value
+-- SubsM {runSubsM :: Context -> Either Error (a, Env)}
+-- SubsM {runSubsM :: Context -> Either Error (Primitive, Env)}
+-- SubsM {runSubsM :: Context -> Either Error (Primitive, Env)}
+-- helper function
+-- should look up the function implementing primitive i.
 
 getFunction :: FunName -> SubsM Primitive
-getFunction name = SubsM (\(e0,pe0) ->  case (lookup name  $ Map.assocs  pe0)  of
-                                           Nothing -> Left ("unbound function:"++name)
+getFunction name = SubsM (\(e0,pe0) ->  case lookup name $ Map.assocs pe0 of
+                                           Nothing -> Left ("unbound function:" ++ name)
                                            Just f -> Right (f,e0))
---data Expr = Number Int
+-- data Expr = Number Int
 --          | String String
 --          | Array [Expr]
 --          | Undefined
@@ -172,19 +173,20 @@ getFunction name = SubsM (\(e0,pe0) ->  case (lookup name  $ Map.assocs  pe0)  o
 --          | Call FunName [Expr]
 --          | Assign Ident Expr
 --          | Comma Expr Expr
---          deriving (Eq, Read, Show)		
+--          deriving (Eq, Read, Show)
 evalExpr :: Expr -> SubsM Value
 evalExpr (Number n) = return (IntVal n)
 evalExpr (String n) = return (StringVal n)
 evalExpr Undefined = return UndefinedVal
 evalExpr TrueConst = return TrueVal
 evalExpr FalseConst = return FalseVal
-evalExpr (Var name) = do x <- getVar name 
-                         return x
-evalExpr (Comma a b) = (evalExpr a) >> (evalExpr b)
-evalExpr (Assign id expr) = do let a = evalExpr expr
-                               fmap (putVar id) a
-                               a
+evalExpr (Var name) = do 
+  getVar name
+evalExpr (Comma a b) = evalExpr a >> evalExpr b
+evalExpr (Assign ident expr) = do
+  let a = evalExpr expr
+  _ <- fmap (putVar ident) a
+  a
 
 -- -- f [Value] -> Either Error Value' with `Value
 -- evalExpr (Call functionName l) = do f <- getFunction functionName
@@ -198,18 +200,21 @@ evalExpr (Assign id expr) = do let a = evalExpr expr
 --                                     -- (result, _) <- (fmap f valList)
 --                                     -- return result
 
+evalExpr (Call fname l) = do
+  funct <- getFunction fname
+  v <- mapM evalExpr l
+  case funct v of
+    Left err -> fail err
+    Right val -> return val
 
-evalExpr (Call fname l) = do funct <- getFunction fname
-                             v <- (mapM evalExpr l)
-                             case (funct v) of 
-                                 Left err -> fail err
-                                 Right (val) -> return val
-				
+-- dummy implemetation not to throw errors
+evalExpr (Array _) = return (IntVal 1)
+evalExpr (Compr _) = return (IntVal 1)
+
 runExpr :: Expr -> Either Error Value
-runExpr expr = do (val,env) <-runSubsM (evalExpr expr) initialContext
-                  return val  
-				  
-				  
-				  
---main = equality [(IntVal 2),(IntVal 1)]
-main = runExpr (Call "===" [(Number 1), (Number 1)])
+runExpr expr = do
+  (val, _) <-runSubsM (evalExpr expr) initialContext
+  return val
+
+-- main = equality [(IntVal 2),(IntVal 1)]
+-- main = runExpr (Call "===" [(Number 1), (Number 1)])
