36b9e4b935971f7f421bf4dc9aa86ef99cf29f64
hubz00 <hubert.zajac@outlook.com>
2017-09-26 10:58:11 +0200

    parsing strins, numbers, ids, a bit of skeleton and tests

--- Assignment_3/src/Parser/Impl.hs
+++ Assignment_3/src/Parser/Impl.hs
@@ -3,25 +3,155 @@ module Parser.Impl where
 import SubsAst
 import Data.Char
 import Text.Parsec.String
+import Control.Applicative
+import Control.Monad
+import Text.Parsec (ParseError)
+import qualified Data.Map as Map
+import Data.Map(Map)
 import qualified Text.Parsec as P
 
 -- can change this if you want, but must be an instance of Show and Eq
-data ParseError = ParseError String
+data CustomParseError = CustomParseError String
                 deriving (Show, Eq)
 
-parseString :: String -> Either ParseError Expr
+type KeyStore = Map String String -- map of keywords
+
+
+keywords :: KeyStore
+keywords = keyStore
+  where keyStore =
+          Map.fromList [ ("true", "true")
+                       , ("false", "false")
+                       , ("for", "for")
+                       , ("undefined", "undefined")
+                       , ("of", "of")
+                       , ("if", "if")
+                       ]
+
+
+-- To move removing whitespaces higher during parsing
+
+cParse :: Parser a -> String -> Either ParseError a
+cParse p = P.parse p ""
+
+lexeme :: Parser a -> Parser a
+lexeme p = do
+           x <- p
+           pWhitespaces
+           return x
+
+parseWWS :: Parser a -> String -> Either ParseError a
+parseWWS p = cParse wrapper
+  where
+    wrapper = do
+        pWhitespaces
+        p
+
+pWhitespaces :: Parser ()
+pWhitespaces = void $ P.many $ P.oneOf " \n\t"
+
+-- ---------------------------------------------------
+
+
+parseString :: String -> Either CustomParseError Expr
 parseString = undefined
 
-int :: Parser Expr
-int = do
-  n <- P.many1 P.digit
+-- ------------ EXPR Start ---------------------------
+
+pExpr :: Parser Expr
+pExpr = pExpr' <|> pExpr1
+
+
+pExpr' :: Parser Expr
+pExpr' = do
+  e0 <- pExpr
+  void $ lexeme $ P.char ','
+  e1 <- pExpr
+  return (Comma e0 e1)
+
+
+pExpr1 :: Parser Expr
+pExpr1 = pTerminal <|> pIdentWrap <|> pParen <|> pExpr1'
+
+pTerminal :: Parser Expr
+pTerminal = pNumber <|> pString <|> pTrue <|> pFalse <|> pUndefined
+
+pIdentWrap :: Parser Expr
+pIdentWrap = undefined
+
+pParen :: Parser Expr
+pParen = undefined
+
+pExpr1' :: Parser Expr
+pExpr1' = undefined
+
+
+
+-- Terminal -> Number | String | "true" | "false" | "undefined"
+
+pNumber :: Parser Expr
+pNumber = do
+  n <- lexeme $ P.many1 P.digit
   return (Number (read n))
 
-ident :: Parser Expr
-ident = do
+pString :: Parser Expr
+pString = lexeme $ do
   h <- firstChar
   t <- P.many nonFirstChar
+  guard (case lookup (h:t) (Map.assocs keywords) of
+                                    Nothing -> True
+                                    Just _ -> False)
+  return (String (h:t))
+  where
+    firstChar = P.satisfy (\a -> isAscii a)
+    nonFirstChar = P.satisfy (\a -> isAscii  a) -- || a `elem` ["\'", "\"", "\n", "\t", "\\"] how to make it skip those signs?
+
+pTrue :: Parser Expr
+pTrue = do
+  val <- pWord
+  guard (val == "true")
+  return TrueConst
+
+pFalse :: Parser Expr
+pFalse = do
+  val <- pWord
+  guard (val == "false")
+  return FalseConst
+
+pUndefined :: Parser Expr
+pUndefined = do
+  val <- pWord
+  guard (val == "undefined")
+  return Undefined
+
+-- --------------------------------------------------------
+
+-- ---------------  pIdentWrap -> Ident Ident'
+
+
+pIdent :: Parser Expr
+pIdent = lexeme $ do
+  h <- firstChar
+  t <- P.many nonFirstChar
+  -- guard () dunno what i wanted to write
   return (Var (h:t))
   where 
     firstChar = P.satisfy (\a -> isLetter a)
     nonFirstChar = P.satisfy (\a -> isDigit a || isLetter a || a == '_')
+
+pIdent' :: Parser Expr
+pIdent' = undefined -- pEmpty -- <|> pAssignent <|> pFunCall todo implement / find out how to represent empty production, cause pEmpty doesn't really work
+
+
+
+-- ------------ Utils -----------------------
+
+pWord :: Parser String
+pWord = do
+  e0 <- lexeme $ P.many1 $ letters
+  return e0
+  where
+    letters = P.satisfy (\a -> isLetter a)
+
+-- pEmpty :: Parser String -- todo Finish
+-- pEmpty = undefined
--- Assignment_3/src/Parser/Tests.hs
+++ Assignment_3/src/Parser/Tests.hs
@@ -7,18 +7,41 @@ import Test.HUnit
 import Text.Parsec 
 
 
-n1 = parse int "error message 1" "123"
-n2 = parse int "error message 2" "231321123"
-n3 = parse int "error message 3" "1"
+n1 = parseWWS pNumber "123    "
+n2 = parseWWS pNumber "   231321123"
+n3 = parseWWS pNumber "1"
+
+i1 = parseWWS pIdent "name"
+i2 = parseWWS pIdent "  n_321"
+i3 = parseWWS pIdent "dsADS_123___"
+
+s1 = parseWWS pString "@DF!#!   "
+s2 = parseWWS pString "_!!  dsadeqw"
+s3 = parseWWS pString "==}'dsdsa22''222"
+s4 = parseWWS pString "true"
+
 -- n4 = pnum "s"
 
 testn1 = TestCase $ assertEqual "Number 1" (Right (Number 123)) (n1)
 testn2 = TestCase $ assertEqual "Number 2" (Right (Number 231321123)) (n2)
 testn3 = TestCase $ assertEqual "Number 3" (Right (Number 1)) (n3)
+
+testi1 = TestCase $ assertEqual "Ident 1" (Right (Var "name")) (i1)
+testi2 = TestCase $ assertEqual "Ident 2" (Right (Var "n_321")) (i2)
+testi3 = TestCase $ assertEqual "Ident 3" (Right (Var "dsADS_123___")) (i3)
+
+tests1 = TestCase $ assertEqual "String 1" (Right (String "@DF!#!   ")) (s1)
+tests2 = TestCase $ assertEqual "String 2" (Right (String "_!!  dsadeqw")) (s2)
+tests3 = TestCase $ assertEqual "String 3" (Right (String "==}'dsdsa22''222")) (s3)
+tests4 = TestCase $ assertEqual "String 4" (Right (String "@DF!#!   ")) (s4) -- todo: how to mah against PerseError - impossibru the constructor is private :c
+
+
 -- testn4 = TestCase $ assertEqual "Number 4" (Right 123) (n1)
 
 
-tests = TestList [TestLabel "Numbers" $ TestList [testn1, testn2, testn3]]
+tests = TestList [TestLabel "Numbers" $ TestList [testn1, testn2, testn3],
+          TestLabel "Ident" $ TestList [testi1, testi2, testi3],
+          TestLabel "String" $ TestList [tests1, tests2, tests3, tests4]]
 
 
 
