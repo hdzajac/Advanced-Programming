5f9af457058d9af65deaa6f217819195b14eaac7
ria.andreea@gmail.com <ria.andreea@gmail.com>
2017-10-02 18:50:50 +0200

    likes

---
+++ Assignment_4/a.js
@@ -0,0 +1,19 @@
+ T Call: (8) friendly([person(kara, [barry, clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver)
+   Call: (8) friendly([person(kara, [barry, clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver) ? creep
+   Call: (9) friendly1([person(kara, [barry, clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], [person(kara, [barry, clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver) ? creep
+   Call: (10) friendly1([person(kara, [barry, clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], [person(kara, [clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver) ? creep
+   Call: (11) friendly1([person(kara, [barry, clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], [person(kara, []), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver) ? creep
+   Call: (12) friendly1([person(kara, [barry, clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], [person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver) ? creep
+   Call: (13) friendly1([person(kara, [barry, clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], [person(bruce, [oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver) ? creep
+   Call: (14) likes([person(kara, [barry, clark]), person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver, bruce) ? creep
+   Call: (15) likes([person(bruce, [clark, oliver]), person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver, bruce) ? creep
+   Call: (16) likes([person(barry, [kara, oliver]), person(clark, [oliver, kara]), person(oliver, [kara])], oliver, bruce) ? creep
+   Call: (17) likes([person(clark, [oliver, kara]), person(oliver, [kara])], oliver, bruce) ? creep
+   Call: (18) likes([person(oliver, [kara])], oliver, bruce) ? creep
+   Call: (19) likes([person(oliver, [])], oliver, bruce) ? creep
+   Call: (20) likes([], oliver, bruce) ? creep
+   Fail: (20) likes([], oliver, bruce) ? creep
+   Fail: (19) likes([person(oliver, [])], oliver, bruce) ? creep
+   Redo: (18) likes([person(oliver, [kara])], oliver, bruce) ? creep
+   Call: (19) likes([], oliver, bruce) ? creep
+   Fail: (19) likes([], oliver, bruce) ?
